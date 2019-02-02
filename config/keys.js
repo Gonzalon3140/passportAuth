8deda15407f3a36e3f5e8d057e6113abf5a870f5
Gonzalon <40928276+Gonzalon3140@users.noreply.github.com>
2019-02-02 11:40:27 -0800

    first commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MongoURI: "mongodb+srv://tony_g:tony123@passport-0fkgz.mongodb.net/test?retryWrites=true"
+}
+
+
+// mongoose.connect('mongodb://user:password@host/yourDB?authSource=yourDB&w=1') this work for me . &w=1 is important
